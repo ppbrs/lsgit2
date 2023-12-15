ac4c5533a471b0d6adcb57953cea1cea996ba124
ppbrs <meemkcfr@proton.me>
2023-12-15 11:24:15 -0800

    Reorder logic for better reporting.

--- src/main.rs
+++ src/main.rs
@@ -67,58 +67,112 @@ fn main() {
     };
 
     let mut check_dir_cnt: i32 = 0;
-    let mut git_dir_vec: Vec<std::path::PathBuf> = Vec::new();
-    check_dir(start_dir.as_ref().unwrap().as_path(), &mut check_dir_cnt, &mut git_dir_vec);
-    git_dir_vec.sort();use std::str;
+    let mut repo_abs_paths: Vec<std::path::PathBuf> = Vec::new();
+    check_dir(start_dir.as_ref().unwrap().as_path(), &mut check_dir_cnt, &mut repo_abs_paths);
+    repo_abs_paths.sort();use std::str;
 
     match check_dir_cnt {
         0 => println!("\tNo directories were checked."),
         1 => println!("\t1 directory was checked."),
         _ => println!("\t{} directories were checked.", check_dir_cnt)
     }
-    let num_repos = git_dir_vec.len();
+    let num_repos = repo_abs_paths.len();
     match num_repos {
         0 => println!("\tNo git repositories were found."),
         1 => println!("\t1 git repository was found."),
         _ => println!("\t{} git repositories were found.", num_repos)
     }
-    println!();
 
     let start_dir_comp: Vec<&OsStr> = start_dir.as_ref().unwrap().iter().collect();
 
     // Fetch repository updates in the background.
-    for git_dir in git_dir_vec.iter() {
-        let _ = Command::new("git").args(["-C", git_dir.to_str().unwrap(), "fetch"])
+    for repo_path_abs in repo_abs_paths.iter() {
+        let _ = Command::new("git").args(["-C", repo_path_abs.to_str().unwrap(), "fetch"])
             .stdout(Stdio::piped()).stderr(Stdio::piped()).spawn();
     }
 
-    for git_dir in git_dir_vec.iter() {
+    // Collect repo statuses.
+    let mut repo_statuses: Vec<String> = Vec::new();
+    for repo_abs_path in repo_abs_paths.iter() {
 
-        let status_stdout = Command::new("git").args(["-C", git_dir.to_str().unwrap(), "status", "--branch", "--short"]).output().unwrap().stdout;
+        let status_stdout = Command::new("git").args(["-C", repo_abs_path.to_str().unwrap(), "status", "--branch", "--short"]).output().unwrap().stdout;
         let status_str = str::from_utf8(&status_stdout).unwrap();
         let status_str: Vec<&str> = status_str.split("\n").collect();
         let status_str = status_str[0]; // For example "## HEAD (no branch)", or "## master...origin/master".
+        repo_statuses.push(status_str.to_string());
+    }
 
-        let git_dir_comp: Vec<&OsStr> = git_dir.iter().collect();
-        let mut git_dir = std::path::PathBuf::new();
-        for n in start_dir_comp.len()..git_dir_comp.len() {
-            git_dir.push(git_dir_comp[n]);
+    // Collect relative repo paths.
+    let mut repo_rel_paths: Vec<std::path::PathBuf> = Vec::new();
+    for repo_abs_path in repo_abs_paths.iter() {
+
+        // Remove parent path from the repository path.
+        let repo_abs_path_comps: Vec<&OsStr> = repo_abs_path.iter().collect();
+        let mut repo_rel_path = std::path::PathBuf::new();
+        for n in start_dir_comp.len()..repo_abs_path_comps.len() {
+            repo_rel_path.push(repo_abs_path_comps[n]);
         }
+        repo_rel_paths.push(repo_rel_path);
+    }
 
-        // git_dir is an absolute path to the git repository, for example /home/boris/projects/lsgit2
-        // start_dir is an absolute path to the search directory, for example /home/boris/projects/
-        let mut indent = String::from("");
-        for i in start_dir_comp.len()..(git_dir_comp.len() - 1) {
-            // indent.push('\t');
-            indent += git_dir_comp[i].to_str().unwrap();
-            indent += "/";
+    let mut selected: Vec<bool> = Vec::new();
+    let mut current_dir_is_repo = false;
+    let mut current_dir_repo_name: String = "".to_string();
+    for n in 0..repo_rel_paths.len() {
+        let repo_rel_path: &std::path::PathBuf= &repo_rel_paths[n];
+        let repo_status: &str = &repo_statuses[n];
+        if regex_pattern.is_match(repo_rel_path.to_str().unwrap()) || regex_pattern.is_match(repo_status) {
+            selected.push(true);
+            let repo_rel_path_comps: Vec<&OsStr> = repo_rel_path.iter().collect();
+            if repo_rel_path_comps.len() == 0 {
+                current_dir_is_repo = true;
+                let repo_abs_path = &repo_abs_paths[n];
+                let repo_abs_path_comps: Vec<&OsStr> = repo_abs_path.iter().collect();
+                current_dir_repo_name = repo_abs_path_comps[repo_abs_path_comps.len() - 1].to_str().unwrap().to_string();
+            }
+        } else {
+            selected.push(false);
         }
+    }
 
-        // Filter git directories and git status strings.
-        if regex_pattern.is_match(git_dir.to_str().unwrap()) || regex_pattern.is_match(status_str) {
-            // let git_dir_str = git_dir.to_str().unwrap();
-            let git_dir_str = git_dir_comp[git_dir_comp.len()-1].to_str().unwrap();
-            println!("{}{} {}.", indent, git_dir_str.white().bold(), status_str.green());
+    let num_selected = selected.iter().filter(|&n| *n == true).count();
+    match num_selected {
+        0 => println!("\tNo git repositories were selected."),
+        1 => println!("\t1 git repository was selected."),
+        _ => println!("\t{} git repositories were selected.", num_selected)
+    }
+    if current_dir_is_repo {
+        println!("\tCurrent directory ({}) is a repository.", current_dir_repo_name);
+    }
+    println!();
+
+    // Sanity check.
+    if repo_abs_paths.len() != repo_rel_paths.len() || repo_abs_paths.len() != selected.len() || repo_abs_paths.len() != repo_statuses.len() {
+        panic!("There is a problem with logic.");
+    }
+
+    for n in 0..repo_rel_paths.len() {
+        if selected[n] {
+            let repo_rel_path: &std::path::PathBuf= &repo_rel_paths[n];
+            let repo_status: &str = &repo_statuses[n];
+
+            let repo_rel_path_comps: Vec<&OsStr> = repo_rel_path.iter().collect();
+            let mut indent: String = String::from("");
+            let repo_dir_str: &str;
+            if repo_rel_path_comps.len() > 0 {
+                indent += &current_dir_repo_name;
+                indent += "/";
+                for i in 0..(repo_rel_path_comps.len() - 1) {
+                    indent += repo_rel_path_comps[i].to_str().unwrap();
+                    indent += "/";
+                }
+                repo_dir_str = repo_rel_path_comps[repo_rel_path_comps.len()-1].to_str().unwrap();
+            } else {
+                // The starting directory is a repository itself.
+                let repo_abs_path_comps: Vec<&OsStr> = repo_abs_paths[n].iter().collect();
+                repo_dir_str = repo_abs_path_comps[repo_abs_path_comps.len() - 1].to_str().unwrap();
+            }
+            println!("{}{} {}.", indent, repo_dir_str.white().bold(), repo_status.green());
         }
     }
 }
